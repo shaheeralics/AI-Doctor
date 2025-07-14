dd07f1b891c010272cfc8bd2c6d80db990d28abf
Your Name <you@example.com>
2025-07-14 16:45:09 +0500

    .

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,33 @@ import 'screens/chat_page.dart';
 import 'screens/report_upload_page.dart';
 import 'screens/history_page.dart';
 import 'screens/settings_page.dart';
+import 'screens/login_screen/login_screen.dart';
+import 'utils/constants.dart';
 
 void main() {
-  runApp(AIDoctorApp());
+  runApp(const AIDoctorApp());
 }
 
 class AIDoctorApp extends StatelessWidget {
+  const AIDoctorApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'AI Doctor',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        scaffoldBackgroundColor: kBackgroundColor,
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: kPrimaryColor,
+              fontFamily: 'Montserrat',
+            ),
         primarySwatch: Colors.teal,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomePage(),
+      home: const LoginScreen(),
       routes: {
+        '/home': (context) => HomePage(),
         '/chat': (context) => ChatPage(),
         '/upload': (context) => ReportUploadPage(),
         '/history': (context) => HistoryPage(),
