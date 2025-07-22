170fcb7c921d5500dfefb0dac84dbf4f4c9d8708
shaheerali.office <shaheerali.office@gmail.com>
2025-07-22 14:57:40 +0000

    .

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:ai_doctor_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(AIDocConnectApp());
+    await tester.pumpWidget(const AIDocConnectApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
