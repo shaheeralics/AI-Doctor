e865b12020ac0ec23a4fd4b338e71db22f8c28ce
shaheerali.office <shaheerali.office@gmail.com>
2025-07-22 14:53:48 +0000

    .

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:myapp/lib/main.dart';
+import 'package:ai_doctor_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
