227f708c100e629a5e02c4feb772642881470393
shaheerali.office <shaheerali.office@gmail.com>
2025-07-22 15:22:14 +0000

    .

--- lib/screens/login_screen/login_screen.dart
+++ lib/screens/login_screen/login_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-// Correct import path for ChatScreen
 import '../chat_screen/chat_screen.dart';
 
 class LoginScreen extends StatefulWidget {
